117ce05c048d3623ab4f9404aec0a1c697265d1b
jalletto <joshalletto@gmail.com>
2019-06-17 18:33:40 -0500

    wip

--- moviesByPhone/settings.py
+++ moviesByPhone/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'moviesByPhone',
-        'USER': 'admin',
+        # 'USER': 'admin',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
